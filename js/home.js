519c9f3b473ef11b85418f3fbb2b4cf4ea3a0639
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-10-25 00:43:58 -0300

    finish=home

--- js/home.js
+++ js/home.js
@@ -1,38 +1,22 @@
 function showPreview() {
-      const _transitionHome = document.getElementById('_start-transition-home');
+      const _animationSplash = document.getElementById('_animation-splash');
+      const _animationName = document.getElementById('_animation-name');
       const load = document.getElementById('load');
       const body = document.getElementById('body');
       const mainHome = document.getElementById('main-home');
-      const _startTransitionCol = document.getElementById('_start-transition-col');
-      const _startTransitionCol1 = document.querySelector('._start-transition-col > .col:nth-child(1) > div');
-      const _startTransitionCol2 = document.querySelector('._start-transition-col > .col:nth-child(2) > div');
-      const _startTransitionCol3 = document.querySelector('._start-transition-col > .col:nth-child(3) > div');
-      const _startTransitionCol4 = document.querySelector('._start-transition-col > .col:nth-child(4) > div');
-      const _startTransitionCol5 = document.querySelector('._start-transition-col > .col:nth-child(5) > div');
+      setTimeout(() => {
+            _animationName.classList.add('_end-animation-name');
+      }, 3200)
+      setTimeout(() => {
+            _animationName.classList.add('d-none');
+            _animationSplash.classList.add('d-block');
+            _animationSplash.classList.add('_animation-splash');
+      }, 3400)
       setTimeout(() => {
             load.classList.add('d-none');
-            _transitionHome.classList.add('top-0');
-            mainHome.classList.add('show-main-home');
-      }, 40)
-      setTimeout(() => {
-            _transitionHome.classList.remove('_start-transition-home');
-            _transitionHome.classList.remove('top-0');
-            _transitionHome.classList.add('_end-transition-home');
-      }, 49)
-      setTimeout(() => {
-            _startTransitionCol3.classList.remove('_start-transition-col-3');
-            _startTransitionCol3.classList.add('h-600');
-            _startTransitionCol3.classList.add('_start-zoom-col-3');
-      }, 55)
-      setTimeout(() => {
-            _startTransitionCol.classList.add('d-none');
             mainHome.classList.add('d-block');
-      }, 67)
-
-
-      setTimeout(() => {
-            _transitionHome.classList.add('d-none');
-      }, 77)
+            mainHome.classList.add('show-main-home');
+      }, 5400)
       body.style.cursor = 'none';
 
 
