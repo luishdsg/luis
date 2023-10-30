550f36c93d1951b4c3bc35443a4e327683c0f286
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-10-30 01:34:18 -0300

    update

--- js/about.js
+++ js/about.js
@@ -14,28 +14,39 @@ function _scrollAbout() {
 }
 
 function _animationAboutInfo() {
+    const aboutInfo = document.querySelector('.about-info');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                $(document).ready(function () {
+                    animation();
+                }, 10);
 
-
-
+                $('.about-info-button').click(function () {
+                    animation();
+                });
+                function animation() {
+                    var title1 = new TimelineMax();
+                    var audio = document.getElementById("about-learn");
+                    audio.play();
+                    title1.to(".about-info-button", 0, { visibility: 'hidden', opacity: 0 })
+                    title1.staggerFromTo(".about-info-title span", 0.5,
+                        { ease: Back.easeOut.config(1.7), opacity: 0, bottom: -30 },
+                        { ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0 }, 0.05);
+                    title1.to(".about-info-button", 0.2, { visibility: 'visible', opacity: 1 })
+                }
+                observer.disconnect();
+            }
+        });
+    });
+    observer.observe(aboutInfo);
     $(document).ready(function () {
         $(".about-info-title").lettering();
         $(".about-info-button").lettering();
     });
-    $(document).ready(function () {
-        animation();
-    }, 10);
 
-    $('.about-info-button').click(function () {
-        animation();
-    });
-    function animation() {
-        var title1 = new TimelineMax();
-        title1.to(".about-info-button", 0, { visibility: 'hidden', opacity: 0 })
-        title1.staggerFromTo(".about-info-title span", 0.5,
-            { ease: Back.easeOut.config(1.7), opacity: 0, bottom: -30 },
-            { ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0 }, 0.05);
-        title1.to(".about-info-button", 0.2, { visibility: 'visible', opacity: 1 })
-    }
+
+
 }
 
 document.addEventListener("scroll", function () {
@@ -56,6 +67,14 @@ function flat() {
 }
 function clicked() {
     var audio = document.getElementById("clicked");
+    audio.play();
+}
+function pc1() {
+    var audio = document.getElementById("pc-1");
+    audio.play();
+}
+function pc2() {
+    var audio = document.getElementById("pc-2");
     audio.play();
 }
 
