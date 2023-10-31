42903a10bfd3aff9a14fdde70c40eb8773a5c43b
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-10-31 06:00:47 -0300

    sound

--- js/about.js
+++ js/about.js
@@ -1,6 +1,8 @@
 // Adiciona um ouvinte de evento de rolagem ao documento
 function _scrollAbout() {
     const about = document.querySelector('.about');
+    const aboutCloseBtn = document.querySelector(".about-close-btn");
+    const aboutSound = document.querySelector('.about-sound');
     if (window.scrollY > 50) {
         about.classList.remove('top-100');
         about.classList.add('show-page');
@@ -8,20 +10,22 @@ function _scrollAbout() {
     } else {
         about.classList.add('top-100');
         about.classList.remove('z-1000');
-        about.classList.remove('show-page');
+        aboutSound.style.display = 'none';
+        aboutCloseBtn.style.display = 'none';
     }
     console.log(window.scrollY)
 }
 
 function _animationAboutInfo() {
     const aboutInfo = document.querySelector('.about-info');
+    const aboutCloseBtn = document.querySelector(".about-close-btn");
+    const aboutSound = document.querySelector('.about-sound');
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 $(document).ready(function () {
                     animation();
                 }, 10);
-
                 $('.about-info-button').click(function () {
                     animation();
                 });
@@ -29,6 +33,8 @@ function _animationAboutInfo() {
                     var title1 = new TimelineMax();
                     var audio = document.getElementById("about-learn");
                     audio.play();
+                    aboutSound.style.display = 'block';
+                    aboutCloseBtn.style.display = 'flex';
                     title1.to(".about-info-button", 0, { visibility: 'hidden', opacity: 0 })
                     title1.staggerFromTo(".about-info-title span", 0.5,
                         { ease: Back.easeOut.config(1.7), opacity: 0, bottom: -30 },
@@ -36,17 +42,26 @@ function _animationAboutInfo() {
                     title1.to(".about-info-button", 0.2, { visibility: 'visible', opacity: 1 })
                 }
                 observer.disconnect();
+            }else{
+                console.log('sssd')
             }
         });
+       
     });
     observer.observe(aboutInfo);
     $(document).ready(function () {
         $(".about-info-title").lettering();
         $(".about-info-button").lettering();
     });
+}
 
-
-
+function _aboutSound() {
+    const audio = document.getElementById("about-learn");
+    const aboutCloseBtn = document.querySelector(".about-close-btn");
+    const aboutSound = document.querySelector('.about-sound');
+        audio.pause();
+        aboutSound.style.display = 'none';
+        aboutCloseBtn.style.display = 'none';
 }
 
 document.addEventListener("scroll", function () {
