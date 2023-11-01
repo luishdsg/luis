4700c31ae67ea66a84e47a5e1516f535870d4533
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-11-01 05:44:31 -0300

    Merge remote-tracking branch 'origin/main' into origin/temporary

---
+++ src/app/components/home/banner/banner.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {trigger, style, animate, transition, stagger, query } from "@angular/animations"
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss'],
+  animations: [
+    trigger('bannerTrigger', [
+      transition(":enter", [
+        query("*", [
+          style({ opacity: 0, transform: "translateX(-50px)" }),
+          stagger(50, [
+            animate(
+              "250ms cubic-bezier(0.35, 0, 0.25, 1)",
+              style({ opacity: 1, transform: "none" })
+            )
+          ])
+        ])
+      ])
+    ])
+  ]
+})
+export class BannerComponent implements OnInit {
+
+  
+
+  constructor(
+    public router: Router
+  ) { }
+
+  ngOnInit(): void { 
+  }
+  
+  Download(){
+    this.router.navigate(['/vc']);
+  }
+
+}
---
+++ src/app/components/home/formation/formation.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formation',
+  templateUrl: './formation.component.html',
+  styleUrls: ['./formation.component.scss']
+})
+export class FormationComponent implements OnInit {
+  
+  active = 0
+  
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
